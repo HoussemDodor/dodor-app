9a152b90c90a76cc71623e3b53fd03e6dce3dea9
Houssem Dodor <houssem@dodor.nl>
2023-10-03 17:05:42 +0200

    commiting to remote to resume work at home

--- src/Login.js
+++ src/Login.js
@@ -11,33 +11,49 @@ const Login = () => {
     console.log(userName);
   }, [userName]);
 
+  useEffect(() => {
+    console.log(pwd);
+  }, [pwd]);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    // TODO: Validate username and password
+    console.log("User: ", userName, "   pwd: ", pwd);
+  }
+
   return (
     <section className="flex justify-center items-center h-screen bg-blue-300">
       <div className="w-96 p-6 shadow-lg bg-white rounded-md">
-        <h1 className="text-3xl block text-center font-semibold">Log In</h1>
-        <hr className="mt-3" />
-        <div className="mt-3">
-          <label className="block text-base mb-2">Gebruikersnaam:</label>
-          <input
-            type="text"
-            id="username"
-            className="border w-full rounded-md text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
-            placeholder="Email of Gebruikersnaam..."
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
-        <div className="mt-3">
-          <label className="block text-base mb-2">Wacthwoord:</label>
-          <input
-            type="password"
-            id="password"
-            className="border w-full rounded-md text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
-            placeholder="Uw wachtwoord..."
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
+        <form onSubmit={handleSubmit}>
+          <h1 className="text-3xl block text-center font-semibold">Log In</h1>
+          <hr className="mt-3" />
+          <div className="mt-3">
+            <label className="block text-base mb-2">Gebruikersnaam:</label>
+            <input
+              type="text"
+              id="username"
+              className="border w-full rounded-md text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="Email of Gebruikersnaam..."
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="mt-3">
+            <label className="block text-base mb-2">Wachtwoord:</label>
+            <input
+              type="password"
+              id="password"
+              className="border w-full rounded-md text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="Uw wachtwoord..."
+              onChange={(e) => setPwd(e.target.value)}
+            />
+          </div>
 
-        <button className="border-2 border-blue-700 bg-blue-700 text-white py-1 w-full rounded-md hover:bg-blue-300 mt-3" type="submit">Log in</button>
+          <button
+            className="border-2 border-blue-700 bg-blue-700 text-white py-1 w-full rounded-md hover:bg-blue-300 mt-3" type="submit"
+          >
+            Log in
+          </button>
+        </form>
       </div>
     </section>
   );
