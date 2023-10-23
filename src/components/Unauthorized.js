2ce4d8c4cf635f0a3e4a3726db7bae296272d8f7
Houssem Dodor <365095@student.fontys.nl>
2023-10-23 19:30:15 +0200

    Added Login Button

--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Unauthorized = () => {
   return (
-    <div>Unauthorized</div>
-  )
-}
+    <div>
+      <h1>Unauthorized</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+};
 
-export default Unauthorized
+export default Unauthorized;
